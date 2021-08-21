74267616def751c1a9c144ac7c3c9c43d42202d2
rhaidiz <rhaidiz@outlook.com>
2021-08-21 15:49:30 +0200

    Implement HTTPS support

--- mitmproxy.go
+++ mitmproxy.go
@@ -1,16 +1,23 @@
 package yves
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"crypto/tls"
+	"crypto/x509"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
+	"net"
 	"net/http"
 	"net/url"
 	"sync"
 	"time"
 )
+
+var okHeader = "HTTP/1.1 200 OK\r\n\r\n"
 
 type Proxy struct {
 	// The Transport used by the proxy and the remote host
@@ -26,12 +33,18 @@ type Proxy struct {
 	HandleResponse func(int64, *http.Request, *http.Response)
 
 	// Session is used to count the number of requests received
-	// so that it is possible to correlate requests and responses from the handlers
+	// so that it is possible to correlate requests and responses from the handlers.
 	session      int64
 	sessionMutex sync.Mutex
+
+	// CaKey and CaCert are, respectively the proxy TLS private
+	// key and certificate in PEM format.
+	CaKey  []byte
+	CaCert []byte
 }
 
 func GetDefault() *Proxy {
+	certs = make(map[string]*tls.Certificate)
 	cl := &http.Client{
 		Transport: &http.Transport{},
 		Timeout:   time.Second * 10}
@@ -40,6 +53,7 @@ func GetDefault() *Proxy {
 }
 
 func GetWithCustomClient(cl *http.Client) *Proxy {
+	certs = make(map[string]*tls.Certificate)
 	return &Proxy{httpClient: cl}
 }
 
@@ -76,6 +90,8 @@ func (p *Proxy) ServeHTTP(wrt http.ResponseWriter, req *http.Request) {
 		}
 
 		// Forward the request to the remote host
+		// RequestURI will contain the Request Target
+		// https://datatracker.ietf.org/doc/html/rfc7230#section-5.3.2
 		resp, err := p.forwardReq(ctx, req, req.RequestURI)
 
 		if err != nil {
@@ -91,13 +107,52 @@ func (p *Proxy) ServeHTTP(wrt http.ResponseWriter, req *http.Request) {
 		}
 
 	} else {
-		// CONNECT not yet supported
-		// dial to remote host
-		// send 200 ok to client
-		// startTLS with client
-		// read request from connection with client
-		// forward request
-		// forward response
+		// Some usefull documentation
+		// https://datatracker.ietf.org/doc/html/rfc2817#section-5.2
+		// For being compliant with the RFC, the proxy should first
+		// perform a dial to the remote destination host and *then* send
+		// a 200OK to the client. However, I have no idea how to do this
+		// while leveraging the convinience of Transport provided by Go.
+		// So for know, I will knowingly violate the RFC.
+
+		// Save the destinationHost along with the scheme.
+		destinationHost := fmt.Sprintf("https://%s", req.RequestURI)
+
+		// Answer with a 200OK to the client.
+		clientConn.Write([]byte(okHeader))
+
+		// Start a TLS connection with the client.
+		clientConn := p.startTlsWithClient(clientConn)
+		defer clientConn.Close()
+
+		clientTlsReader := bufio.NewReader(clientConn)
+		for !isEob(clientTlsReader) {
+
+			reqClone := req.Clone(context.TODO())
+			if err != nil {
+				HttpError(wrt, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			req, err := http.ReadRequest(clientTlsReader)
+			if err != nil {
+				log.Printf("Not an HTTP request: %s\n", err)
+			}
+
+			resp, err := p.forwardReq(ctx, req, destinationHost)
+			if err != nil {
+				HttpError(clientConn, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			// Do I need to have a write buffer for the connection with the client??
+			error := p.forwardResp(ctx, resp, clientConn, reqClone)
+			if error != nil {
+				HttpError(clientConn, error.Error(), http.StatusInternalServerError)
+				return
+			}
+			return
+		}
+		return
 	}
 }
 
@@ -120,7 +175,8 @@ func (p *Proxy) forwardReq(ctx context.Context, clientRequest *http.Request, des
 		return nil, err
 	}
 
-	clientRequest.URL = u
+	clientRequest.URL.Scheme = u.Scheme
+	clientRequest.URL.Host = u.Host
 	return p.httpClient.Do(clientRequest)
 }
 
@@ -142,4 +198,43 @@ func HttpError(conn io.Writer, er string, code int) {
 	rsp.Header.Add("Content-Type", "text/plain; charset=utf-8")
 	rsp.Header.Add("X-Content-Type-Options", "nosniff")
 	rsp.Write(conn)
+}
+
+// startTlsWithClient starts a TLS connection with the client.
+func (p *Proxy) startTlsWithClient(down net.Conn) net.Conn {
+
+	tlfConf := new(tls.Config)
+	// https://pkg.go.dev/crypto/tls#Config
+	// GetCertificate returns a Certificate based on the given
+	// ClientHelloInfo. It will only be called if the client supplies SNI
+	// information or if Certificates is empty.
+	tlfConf.GetCertificate = func(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+		// get CA key pair
+		CA, err := tls.X509KeyPair(p.CaCert, p.CaKey)
+		if err != nil {
+			log.Fatalf("Cannot parse provided CA key pair %s\n", err)
+		}
+		// get CA certificate
+		CA.Leaf, err = x509.ParseCertificate(CA.Certificate[0])
+		if err != nil {
+			log.Fatalf("Cannot parse CA certificate: %s\n", err)
+		}
+		return getCert(CA, hello.ServerName)
+	}
+
+	// perform a TLS connection with the client.
+	c := tls.Server(down, tlfConf)
+	if err := c.Handshake(); err != nil {
+		log.Printf("Server Handshake error: %v\n", err)
+	}
+	return c
+}
+
+// isEob check is there's something else to read from the buffer.
+func isEob(r *bufio.Reader) bool {
+	_, err := r.Peek(1)
+	if err != nil {
+		return true
+	}
+	return false
 }
